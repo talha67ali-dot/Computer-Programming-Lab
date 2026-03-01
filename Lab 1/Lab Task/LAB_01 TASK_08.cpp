ecda8e4ecfe36ee248fa05a53afc3df54e01db39
Talha Ali <talha67ali@gmail.com>
2026-03-02 00:51:09 +0500

    Add files via upload

---
+++ Lab 1/Lab Task/LAB_01 TASK_08.cpp
@@ -0,0 +1,16 @@
+// Name: Talha Ali
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0728
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float aB = 10.2;  // Declaring float variable
+
+    cout << aB;  // Printing value of aB
+
+    return 0;  // Program ends successfully
+}
